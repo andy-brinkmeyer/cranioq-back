af3153c1fab9f3b5da8e598692cf3c50330b7a21
Andy Brinkmeyer <andy.brinkmeyer@gmail.com>
2020-02-29 15:58:59 +0000

    implemented put request for user data

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.exceptions import ValidationError
@@ -6,10 +7,13 @@ from rest_framework.exceptions import ValidationError
 
 class LoginView(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
-        user_data = {
-            'username': request.data['email'],
-            'password': request.data['password']
-        }
+        try:
+            user_data = {
+                'username': request.data['email'],
+                'password': request.data['password']
+            }
+        except KeyError:
+            return Response({'error_message': 'Wrong data format.'}, status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
         if user_data['username'] == '' or user_data['password'] == '':
             raise ValidationError({'error_message': 'One or more fields were left empty.'})
--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.utils import DataError
 
 from .serializers import UserSerializer
 
@@ -15,5 +16,34 @@ class UserView(APIView):
             user = User.objects.get(pk=int(user_id))
         except ObjectDoesNotExist:
             return Response({'error_message': 'This user does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+    @staticmethod
+    def put(request, user_id):
+        try:
+            user = User.objects.get(pk=int(user_id))
+        except ObjectDoesNotExist:
+            return Response({'error_message': 'This user does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            email = request.data['email']
+            first_name = request.data['first_name']
+            last_name = request.data['last_name']
+        except KeyError:
+            return Response({'error_message': 'Wrong data format.'}, status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+        user.email = email
+        user.first_name = first_name
+        user.last_name = last_name
+        try:
+            user.save()
+        except DataError:
+            return Response({'error_message': 'Wrong data format.'}, status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+        response = {
+            'update_success': True,
+            'displayable_message': 'Details changed.'
+        }
+        return Response(response)
