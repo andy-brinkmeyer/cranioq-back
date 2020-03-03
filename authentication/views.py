7b8c23ba72f1ca58bf97a913449eedcadb1e030d
SiobhanHughes <siobhanmahughes@gmail.com>
2020-03-03 13:38:03 +0000

    change settings.py for my path and added to gitignore

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,4 @@
-from rest_framework.response import Response
+python manage.py runserver 8000 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.exceptions import ValidationError
