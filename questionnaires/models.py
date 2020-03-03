54263b689cd4b34e1eefb40ab418feebcb2125c5
Andy Brinkmeyer <andy.brinkmeyer@gmail.com>
2020-03-03 17:14:27 +0000

    changed to EmailField

--- questionnaires/models.py
+++ questionnaires/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # questionnaire model
 class Questionnaire(models.Model):
     patient_id = models.CharField(max_length=100)
-    email = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
 
     class Meta:
         indexes = [models.Index(fields=('patient_id',))]
