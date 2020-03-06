528897c8d078dbe2494d90dd46350cf80a04e68d
Andy Brinkmeyer <andy.brinkmeyer@gmail.com>
2020-03-06 13:38:42 +0000

    changed database to postgres

--- cranioq_back/settings.py
+++ cranioq_back/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from .pg_settings import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -106,22 +108,7 @@ WSGI_APPLICATION = 'cranioq_back.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, 'my.cnf'),
-            'isolation_level': 'read committed',
-            'init_command': 'SET default_storage_engine=INNODB'
-        },
-    }
-}
-
-HOST = 'localhost'
-
-PORT = 3306
-
+# database settings in pg_settings.py
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
