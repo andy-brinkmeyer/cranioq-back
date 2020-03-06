366d95b2f875658034e9ba3459b39526604bff6d
Andy Brinkmeyer <andy.brinkmeyer@gmail.com>
2020-03-06 14:46:20 +0000

    added questionnaire template models

--- questionnaires/admin.py
+++ questionnaires/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Questionnaire
+from .models import Questionnaire, QuestionnaireTemplate, QuestionTemplate
 
 
 # Register your models here.
 admin.site.register(Questionnaire)
+admin.site.register(QuestionnaireTemplate)
+admin.site.register(QuestionTemplate)
--- questionnaires/models.py
+++ questionnaires/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres import fields
 
 
 # questionnaire model
@@ -8,3 +9,16 @@ class Questionnaire(models.Model):
 
     class Meta:
         indexes = [models.Index(fields=('patient_id',))]
+
+
+# models for questionnaire templating
+class QuestionnaireTemplate(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+
+class QuestionTemplate(models.Model):
+    type = models.CharField(max_length=30)
+    question = models.CharField(max_length=500)
+    text = models.TextField()
+    answers = fields.ArrayField(base_field=models.CharField(max_length=200), size=10)
