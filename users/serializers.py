1e3d48d1425ce5ba959afd3562373cb22faae097
Andy Brinkmeyer <andy.brinkmeyer@gmail.com>
2020-03-12 23:12:00 +0000

    reworked profile model

--- users/serializers.py
+++ users/serializers.py
@@ -4,17 +4,11 @@ from django.contrib.auth.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    role = serializers.SerializerMethodField()
+    role = serializers.CharField(source='profile.role.role', read_only=True)
+    clinic_name = serializers.CharField(source='profile.clinic_name', read_only=True)
+    clinic_address = serializers.CharField(source='profile.clinic_address', read_only=True)
+    clinic_postcode = serializers.CharField(source='profile.clinic_postcode', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'role']
-
-    @staticmethod
-    def get_role(obj):
-        if hasattr(obj, 'gp'):
-            return 'gp'
-        elif hasattr(obj, 'specialist'):
-            return 'specialist'
-        else:
-            return 'anon'
+        fields = ['id', 'email', 'first_name', 'last_name', 'role', 'clinic_name', 'clinic_address', 'clinic_postcode']
