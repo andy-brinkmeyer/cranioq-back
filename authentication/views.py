26db02cdf8c483152cf60f1ac714e32dc20e4636
Andy Brinkmeyer <andy.brinkmeyer@gmail.com>
2020-04-18 15:16:57 +0200

    implemented view for changing passwords

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,4 @@
+import re
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.views import ObtainAuthToken, APIView
@@ -5,6 +6,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.exceptions import ValidationError
 from django.contrib.auth.models import AnonymousUser
 from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 
 
 class LoginView(ObtainAuthToken):
@@ -55,3 +57,44 @@ class VerifyView(APIView):
             'id': user.pk,
             'role': role
         })
+
+
+class ChangePassword(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @staticmethod
+    def post(request):
+        try:
+            old_password = request.data['oldPassword']
+            new_password = request.data['newPassword']
+            repeated_password = request.data['repeatNewPassword']
+        except (KeyError, TypeError):
+            return Response({'error_message': 'Invalid data format.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if type(old_password) != str or type(new_password) != str or type(repeated_password) != str:
+            return Response({'error_message': 'The password must be strings'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        user = request.user
+
+        if not user.check_password(old_password):
+            return Response({'error_message': 'Wrong password.'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        if user.check_password(new_password):
+            return Response({'error_message': 'The new password must be different from the old one.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if new_password != repeated_password:
+            return Response({'error_message': 'Passwords do not match.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if not re.search('[0-9]+', new_password):
+            return Response({'error_message': 'The password must contain at least one number.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if len(new_password) < 8:
+            return Response({'error_message': 'The password must be at least 8 characters long.'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        user.set_password(new_password)
+        user.save()
+        return Response(status=status.HTTP_200_OK)
