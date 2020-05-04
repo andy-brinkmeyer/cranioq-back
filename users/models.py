fe952562d5f4eb01fc55f752d9a9f062c36d7164
Andy Brinkmeyer <andy.brinkmeyer@gmail.com>
2020-05-04 21:24:02 +0200

    changed str representation

--- users/models.py
+++ users/models.py
@@ -22,4 +22,4 @@ class Profile(models.Model):
     clinic_postcode = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
-        return 'User: {}'.format(self.user.email)
+        return 'User: {}'.format(self.user.username)
